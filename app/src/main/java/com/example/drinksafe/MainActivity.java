3cdf60383a618b600432b48d5c08a746f60fe29a
KyuTae <rbxo4456@naver.com>
2017-11-20 17:20:25 +0900

    loading 11-20

--- app/src/main/java/com/example/drinksafe/MainActivity.java
+++ app/src/main/java/com/example/drinksafe/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.drinksafe;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/drinksafe/Splash.java
@@ -0,0 +1,34 @@
+package com.example.drinksafe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by MoonKyuTae on 2017-11-20.
+ */
+
+public class Splash extends Activity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(3000);
+                } catch (Exception e) {
+                } finally {
+                    Intent i = new Intent(Splash.this, MainActivity.class);
+                    startActivity(i);
+                    overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
