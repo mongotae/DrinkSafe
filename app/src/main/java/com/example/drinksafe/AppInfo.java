5072f3f22426f89d0e540000e5862eed579a8c1d
KyuTae <rbxo4456@naver.com>
2017-11-27 17:04:44 +0900

    Contact

--- app/src/main/java/com/example/drinksafe/AppInfo.java
+++ app/src/main/java/com/example/drinksafe/AppInfo.java
@@ -11,38 +11,12 @@ import java.util.Comparator;
  */
 
 public class AppInfo {
-    public static interface AppFilter {
-        public void init();
-        public boolean filterApp(ApplicationInfo info);
-    }
     public Drawable icon;
     public String applicationName;
 
     public AppInfo(){
         super();
     }
-    public static final AppFilter THIRD_PARTY_FILTER = new AppFilter() {
-        public void init() {
-        }
-
-        @Override
-        public boolean filterApp(ApplicationInfo info) {
-            if ((info.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
-                return true;
-            } else if ((info.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
-                return true;
-            }
-            return false;
-        }
-    };
-
-    public static final Comparator<AppInfo> ALPHA_COMPARATOR = new Comparator<AppInfo>() {
-        private final Collator sCollator = Collator.getInstance();
-        @Override
-        public int compare(AppInfo object1, AppInfo object2) {
-            return sCollator.compare(object1.applicationName, object2.applicationName);
-        }
-    };
 
     public AppInfo(Drawable icon, String applicationName){
         super();
--- app/src/main/java/com/example/drinksafe/GuardianList.java
+++ app/src/main/java/com/example/drinksafe/GuardianList.java
@@ -3,85 +3,167 @@ package com.example.drinksafe;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Fragment;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by MoonKyuTae on 2017-11-24.
  */
 
-public class GuardianList extends ListActivity implements android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback {
-protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class GuardianList extends ListActivity {
+        String tag = "cap";
+        String [] name, new_name;
+        int count = 0;
+        ListView listView;
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED){
-                if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_CONTACTS)){
-                AlertDialog.Builder builder = new AlertDialog.Builder(GuardianList.this,0);
-                builder.setTitle("Test Alert Dialog")
-                .setMessage("READ_CONTACTS permission is needed to complete the request! would you try to get permission again?")
-                .setNegativeButton("NO",null)
-                .setPositiveButton("Yes",new DialogInterface.OnClickListener(){
-                        public void onClick(DialogInterface dialog, int which){
-                        ActivityCompat.requestPermissions(GuardianList.this, new String[]{Manifest.permission.READ_CONTACTS},123);
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED)
+                {
+                        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_CONTACTS)){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(GuardianList.this,0);
+                                builder.setTitle("Test Alert Dialog")
+                                        .setMessage("READ_CONTACTS permission is needed to complete the request! would you try to get permission again?")
+                                        .setNegativeButton("NO",null)
+                                        .setPositiveButton("Yes",new DialogInterface.OnClickListener(){
+                                                public void onClick(DialogInterface dialog, int which){
+                                                        ActivityCompat.requestPermissions(GuardianList.this, new String[]{Manifest.permission.READ_CONTACTS},123);
+                                                }
+                                        });
+                                builder.show();
                         }
-                });
-        builder.show();
-        }
-        else
-                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_CONTACTS},123);
-        }
-        else{
-                String columns[] = new String[]{
-                ContactsContract.Contacts._ID,
-                ContactsContract.Contacts.DISPLAY_NAME,
-                ContactsContract.Contacts.STARRED,
-                ContactsContract.Contacts.HAS_PHONE_NUMBER
-        };
+                        else
+                                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_CONTACTS},123);
+                } else {
+                        Cursor cursor = getURI();  // 전화번호부 가져오기
+                        int end = cursor.getCount(); // 전화번호부의 갯수 세기
+                        name = new String[end];   // 전화번호부의 이름을 저장할 배열 선언
 
-        String disp[] = new String[]{
-        ContactsContract.Contacts.DISPLAY_NAME,
-        ContactsContract.Contacts.HAS_PHONE_NUMBER};
+                        String[] bbStr = cursor.getColumnNames();
+                        for (int i = 0; i < bbStr.length; i++)
+                                // 각각의 컬럼 이름 확인
+                                Log.e(tag, "ColumnName " + i + " : " + cursor.getColumnName(i));
 
-        int[] colResIds = new int[]{R.id.name};
+                        if (cursor.moveToFirst()) {    //항상 처음에서 시작
+                                do {
+                                        if (!cursor.getString(2).startsWith("01")) // 01로 시작하는 핸펀만
+                                                continue;         // 이멜주소만 있는것은 제외
+                                        // 요소값 얻기
+                                        name[count] = cursor.getString(1);  //이름
+                                        name[count] += "\n";
+                                        name[count] += cursor.getString(2);  //전번
+                                        count++;
+                                } while (cursor.moveToNext());
 
-        ContentResolver cr = getContentResolver();
-        Cursor c = cr.query(ContactsContract.Contacts.CONTENT_URI,
-        columns,ContactsContract.Contacts.STARRED+"=0",
-        null,null);
-        setListAdapter(new SimpleCursorAdapter(this,R.layout.guardian_list,c,disp,colResIds,0));
+                                new_name = new String[count];    //이멜주소 제외한 01번호 가져오기
+                                for (int i = 0; i < count; i++) new_name[i] = name[i];  //복사
+                        }
+                        cursor.close(); // 반드시 커서 닫고
+                        setListAdapter(new ArrayAdapter<String>(this,
+                                android.R.layout.simple_list_item_multiple_choice, // 멀티 쵸이스
+                                new_name));
+                        listView = getListView();         // 리스트뷰
+                        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);  // 반드시 설정해줘야 멀티초이스
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                        String mes = "";
+                                        long num[] = listView.getCheckItemIds();  // 현재 체크된 id들의 배열 리턴
+                                        // 화면표시 - 여기서 얻어온 값들 처리하심됨
+                                        mes = "Selected Item : \n";
+                                        for (int i = 0; i < num.length; i++) {
+                                                mes += i + " : " + name[(int) num[i]];
+                                                mes += "\n";
+                                        }
+                                }
+                        });
+                }
         }
+        public void onRequestPermissionResult(int requestCode,String permission[], int[] grantResults) {
+                if (requestCode == 123) {
+                        Cursor cursor = getURI();  // 전화번호부 가져오기
+                        int end = cursor.getCount(); // 전화번호부의 갯수 세기
+                        name = new String[end];   // 전화번호부의 이름을 저장할 배열 선언
 
+                        String[] bbStr = cursor.getColumnNames();
+                        for (int i = 0; i < bbStr.length; i++)
+                                // 각각의 컬럼 이름 확인
+                                Log.e(tag, "ColumnName " + i + " : " + cursor.getColumnName(i));
+
+                        if (cursor.moveToFirst()) {    //항상 처음에서 시작
+                                do {
+                                        if (!cursor.getString(2).startsWith("01")) // 01로 시작하는 핸펀만
+                                                continue;         // 이멜주소만 있는것은 제외
+                                        // 요소값 얻기
+                                        name[count] = cursor.getString(1);  //이름
+                                        name[count] += "\n";
+                                        name[count] += cursor.getString(2);  //전번
+                                        count++;
+                                } while (cursor.moveToNext());
+
+                                new_name = new String[count];    //이멜주소 제외한 01번호 가져오기
+                                for (int i = 0; i < count; i++) new_name[i] = name[i];  //복사
+                        }
+                        cursor.close(); // 반드시 커서 닫고
+                        setListAdapter(new ArrayAdapter<String>(this,
+                                android.R.layout.simple_list_item_multiple_choice, // 멀티 쵸이스
+                                new_name));
+                        listView = getListView();         // 리스트뷰
+                        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);  // 반드시 설정해줘야 멀티초이스
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                        String mes = "";
+                                        long num[] = listView.getCheckItemIds();  // 현재 체크된 id들의 배열 리턴
+                                        // 화면표시 - 여기서 얻어온 값들 처리하심됨
+                                        mes = "Selected Item : \n";
+                                        for (int i = 0; i < num.length; i++) {
+                                                mes += i + " : " + name[(int) num[i]];
+                                                mes += "\n";
+                                        }
+                                }
+                        });
+                }
         }
-public void onRequestPermissionResult(int requestCode,String permission[], int[] grantResults){
-        if(requestCode == 123){
-                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                        String columns[] = new String[]{
+        private Cursor getURI() {
+                Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+
+                String[] projection = new String[] { // 세개만 프로젝션함
                         ContactsContract.Contacts._ID,
-                        ContactsContract.Contacts.DISPLAY_NAME,
-                        ContactsContract.Contacts.STARRED,
-                        ContactsContract.Contacts.HAS_PHONE_NUMBER
-                        };
-
-        String disp[] = new String[]{
-        ContactsContract.Contacts.DISPLAY_NAME,
-        ContactsContract.Contacts.HAS_PHONE_NUMBER};
-
-        int[] colResIds = new int[]{R.id.name};
-
-        ContentResolver cr = getContentResolver();
-        Cursor c = cr.query(ContactsContract.Contacts.CONTENT_URI,
-        columns,ContactsContract.Contacts.STARRED+"=0",
-        null,null);
-        setListAdapter(new SimpleCursorAdapter(this,R.layout.guardian_list,c,disp,colResIds,0));
-        }
-        }
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                };
+                // 정렬방식 설정
+                String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
+                return managedQuery(uri, projection, null, null, sortOrder);
         }
 }
